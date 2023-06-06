2752eacd7a72a4c09f6f929f66af7de140bbac30
nigashish <ashishn@eupheus.in>
2023-06-06 14:56:59 +0530

    Cloud config configuratio commit

--- src/main/java/com/xamcheck/accelerate/exams/api/controller/ExamsController.java
+++ src/main/java/com/xamcheck/accelerate/exams/api/controller/ExamsController.java
@@ -1,5 +1,6 @@
 package com.xamcheck.accelerate.exams.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,22 @@ import com.xamcheck.accelerate.exams.api.dto.ResponceDto;
 @RestController
 @RequestMapping("/exams")
 public class ExamsController {
-	
+	@Value("${microservice.visor-prs-service.endpoints.endpoint.uri}")
+    private String prsURL;
+	@Value("${microservice.visor-master-service.endpoints.endpoint.uri}")
+    private String masterURL;
+	@Value("${microservice.visor-exams-service.endpoints.endpoint.uri}")
+    private String examsURL;
+	@Value("${microservice.visor-schoolapp-service.endpoints.endpoint.uri}")
+    private String schoolRootURL;
+	@Value("${microservice.visor-teacherapp-service.endpoints.endpoint.uri}")
+    private String teacherRootURL;
+	@Value("${microservice.visor-adminapp-service.endpoints.endpoint.uri}")
+    private String adminRootURL;
 	@GetMapping("/test")
 	public ResponceDto test() {
 		ResponceDto responceDto=new ResponceDto();
+		System.out.println(prsURL+"--"+masterURL+"--"+examsURL+"--"+schoolRootURL);
 		responceDto.setMessage("Just test the Exams Ends Points");
 		return responceDto;
 		
